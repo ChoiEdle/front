2f8949c918138f9c6697a174d9f68c08a8794f2b
choi <cjy1749@naver.com>
2025-12-04 16:35:22 +0900

    12-04 vercel practice

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -40,7 +40,6 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async() => {
       const result = await api.get('https://front-testing-five.vercel.app/data/homeDataimages.json');
-      console.log(result.data);
       setImages(result.data.images);
     };
     dispatch(setProductListAPI());
